cbf62a9da4ce8aabc749ad618a39e7fad4d76590
Luciana Reis <lucianareis91@gmail.com>
2024-10-01 21:57:21 -0300

    Add files via upload

---
+++ calculadora_v2.py
@@ -0,0 +1,41 @@
+saida = ''
+
+def adicao(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b == 0:
+        return "Não foi possível realizar a divisão por 0"
+    else:
+        return a / b
+
+# Função calculadora
+def calculadora(num1, num2, operacao):
+    if operacao == '+' or operacao == 'adicao':
+        resultado = adicao(num1, num2)
+    elif operacao == '-' or operacao == 'subtracao':
+        resultado = subtracao(num1, num2)
+    elif operacao == '*' or operacao == 'multiplicacao':
+        resultado = multiplicacao(num1, num2)
+    elif operacao == '/' or operacao == 'divisao':
+        resultado = divisao(num1, num2)
+    else:
+        resultado = "Operação inválida"
+    return resultado
+
+# Laço while
+while saida.lower() != 'n':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    operacao = input("Digite a operação desejada (+, -, *, / ou o nome da operação): ")
+    
+    resultado = calculadora(num1, num2, operacao)
+    print(f"Resultado da operação: {resultado}")
+    
+    saida = input("Deseja continuar? (S/N): ")
